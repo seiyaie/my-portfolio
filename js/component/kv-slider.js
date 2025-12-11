596a8b18625bb7cdb1b1d735f306bc29581e8f9e
seiya.ie <seiya2342@icloud.com>
2025-12-12 04:02:19 +0900

    feat: kv-sliderのslider-itemをクリックによる画像切り替えと、arrowクリックによる画像切り替えを実装

--- js/component/kv-slider.js
+++ js/component/kv-slider.js
@@ -1,4 +1,43 @@
 export const initKvSlider = () => {
-  const selectedWork = document.querySelector('.js-top-kv-selected-work');
-  const 
+    const selectedWork = document.querySelector(".js-top-kv-selected-work img");
+    const sliderItems = document.querySelectorAll(".js-kv-slider-item--trigger");
+    const nextButton = document.querySelector(".js-kv-scroll-button--next");
+    const prevButton = document.querySelector(".js-kv-scroll-button--prev");
+
+    let activeIndex = 0;
+
+    const updateSelectedWork = (index) => {
+        const item = sliderItems[index];
+
+        const mainSrc = item.dataset.mainSrc;
+        const mainSrcset = item.dataset.mainSrcset;
+        const mainSizes = item.dataset.mainSizes;
+
+        selectedWork.src = mainSrc;
+        selectedWork.srcset = mainSrcset;
+        selectedWork.sizes = mainSizes;
+    };
+
+    sliderItems.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            activeIndex = index;
+            updateSelectedWork(activeIndex);
+        });
+    });
+
+    nextButton.addEventListener("click", () => {
+        activeIndex++;
+        if (activeIndex >= sliderItems.length) {
+            activeIndex = 0;
+        }
+        updateSelectedWork(activeIndex);
+    });
+
+    prevButton.addEventListener("click", () => {
+        activeIndex--;
+        if (activeIndex < 0) {
+            activeIndex = sliderItems.length - 1;
+        }
+        updateSelectedWork(activeIndex);
+    });
 };
--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ import { initThemeSwitch } from "./component/theme-switch.js";
 import { textSlider } from "./component/splide.js";
 import { initSliderListToggle } from "./component/slider-list-toggle.js";
 import { initHamburgerMenu } from "./component/hamburger-menu.js";
+import { initKvSlider } from "./component/kv-slider.js";
 
 initThemeSwitch();
 textSlider();
 initSliderListToggle();
 initHamburgerMenu();
+initKvSlider();
