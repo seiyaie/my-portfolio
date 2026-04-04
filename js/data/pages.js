27782afc0abfff029f907d578ed41ec2d9435f0a
seiya.ie <seiya2342@icloud.com>
2026-04-04 21:45:37 +0900

    refactor: worksのプロジェクト内容をUI Componentsに更新。

--- js/data/pages.js
+++ js/data/pages.js
@@ -2,7 +2,7 @@
 
 export const WORKS_PAGES = [
   { href: "travel-gallery.html", title: "travel-gallery" },
-  { href: "leaf-design-lab.html", title: "leaf-design-lab" },
+  { href: "ui-components.html", title: "ui-components" },
   { href: "my-portfolio.html", title: "my-portfolio" },
   { href: "photographer.html", title: "photographer" },
   { href: "coco.html", title: "coco" },
