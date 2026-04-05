05f25be32e2321ad7b055e15409ea2eb22f12726
seiya.ie <seiya2342@icloud.com>
2026-04-05 13:50:41 +0900

    feat: pages.jsにleaf-design-labの情報を追加

--- js/data/pages.js
+++ js/data/pages.js
@@ -1,12 +1,13 @@
 // date/pages.js
 
 export const WORKS_PAGES = [
-  { href: "travel-gallery.html", title: "travel-gallery" },
-  { href: "ui-components.html", title: "ui-components" },
-  { href: "my-portfolio.html", title: "my-portfolio" },
-  { href: "photographer.html", title: "photographer" },
-  { href: "coco.html", title: "coco" },
-  { href: "matsuyama-clinic.html", title: "matsuyama-clinic" },
-  { href: "moshashugyo-media.html", title: "moshashugyo-media" },
-  { href: "migliore.html", title: "migliore" },
+    { href: "travel-gallery.html", title: "travel-gallery" },
+    { href: "leaf-design-lab.html", title: "leaf-design-lab" },
+    { href: "ui-components.html", title: "ui-components" },
+    { href: "my-portfolio.html", title: "my-portfolio" },
+    { href: "photographer.html", title: "photographer" },
+    { href: "coco.html", title: "coco" },
+    { href: "matsuyama-clinic.html", title: "matsuyama-clinic" },
+    { href: "moshashugyo-media.html", title: "moshashugyo-media" },
+    { href: "migliore.html", title: "migliore" },
 ];
